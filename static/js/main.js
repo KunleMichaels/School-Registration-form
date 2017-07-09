2938289aa05192ce54c17b5ebff31001b07f788e
Ahameed01 <ahameedoye@gmail.com>
2017-07-09 18:44:29 +0100

    I'm hyper

--- static/js/main.js
+++ static/js/main.js
@@ -17,7 +17,7 @@ function validate()
  }
    var passw = /^(?=.*[A-Z])\w{6,}$/;
    var pass1 = document.getElementById("password").value;
-   if(pass1 == "" || !pass1.match(passw) || pass1.match(/^Password$/i))
+   if(pass1 == "" || !pass1.match(passw) || pass1.match("Password"))
    {
      document.SchoolRegistration.password.focus();
      alert("Password is Invalid!");
