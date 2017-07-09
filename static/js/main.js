fc1218130dc26ce9c9ad3966ddfa3dd1dacce0ee
KunleMichaels <ksquare267@gmail.com>
2017-07-09 02:05:28 +0100

    09/07/17

--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,41 @@
-
-/*var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-$(document).ready(function(){
-
-});
-*/
-
 function validate()
 {
    if( document.SchoolRegistration.schoolname.value == "" )
    {
      alert( "Please provide your UserName!" );
-     document.SchoolRegistration.schoolname.focus() ;
+     document.SchoolRegistration.schoolname.focus();
      return false;
    }
-
-   if( document.SchoolRegistration.psw.value == ""  ||
-     document.SchoolRegistration.psw.value.length < 6)
+   var emails = document.SchoolRegistration.email.value;
+  atpos = emails.indexOf("@");
+  dotpos = emails.lastIndexOf(".");
+   if (emails == "" || atpos < 1 || ( dotpos - atpos < 2 ))
+ {
+     alert("Please enter correct email ID")
+     document.SchoolRegistration.email.focus() ;
+     return false;
+ }
+   var passw = /^(?=.*[A-Z])\w{6,}$/;
+   var pass1 = document.getElementById("password").value;
+   if(pass1 == "" || !pass1.match(passw) || pass1.match(/^Password$/i))
    {
-     alert( "Please Password Shouldn't be Less than 6 characters" );
-     document.SchoolRegistration.psw.focus() ;
-     return false;
+     document.SchoolRegistration.password.focus();
+     alert("Password is Invalid!");
+    return false;
    }
-   if( $("#Password").val() != $("#password2").val() ||
-    document.SchoolRegistration.psw-repeat.value == "")
+   var pass2 = document.getElementById("password2").value;
+   if( pass2 != pass1)
    {
      alert( "Please Password Must Match!" );
-     document.SchoolRegistration.psw-repeat.focus() ;
      return false;
    }
-   if( document.SchoolRegistration.mobilenumber.value == "" )
+   if( document.SchoolRegistration.mobilenumber.value == "" ||
+           isNaN( document.SchoolRegistration.mobilenumber.value) ||
+           document.SchoolRegistration.mobilenumber.value.length < 10 )
    {
-     alert( "Please provide your Mobilenumber in the Format!" );
+     alert( "Please provide a Mobile No in the country code format excluding the '+' symbol e.g '2348000000000'" );
      document.SchoolRegistration.mobilenumber.focus() ;
      return false;
    }
-
+   return true;
 }
